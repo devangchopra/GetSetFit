2fe0d58f882c864f827102ae9e673bee7e2f56fd
devangchopra <65708528+devangchopra@users.noreply.github.com>
2020-10-09 15:01:42 +0530

    Properly Defined

--- helper_functions/bmi_body_type_solution.py
+++ helper_functions/bmi_body_type_solution.py
@@ -1,35 +1,47 @@
-def bmi(name,weight,height):
+def bmi(weight,height):
+    '''
+    Fution is created to find the bmi , tell the status and solution
+    
+    Input :: Weight , Height
+    
+    Output :: 
+    BMI -- weight/(height*height)
+    Status -- (Under-weight, Normal , Over-Weight ,Obese)
+    Solution -- (Increase(Calorie Surplus),Maintain, Decrease(Clorie-Defficiet))
+    
+    '''
     #- This function is used to calculate bmi of user
-    # x=bmi
-    x=weight/(height*height)
-    print("Hello",name)
-    print("Your BMI is ",x)
+    
+    bmi=weight/(height*height)
+    #print("Hello",name)
+    #print("Your BMI is ",x)
     #-------------------------
     #to get the body situation
-    print("\n")
-    print(name,"your body situation is")
-    if(x<=18.5):
-        print("Under-Weight")
-    elif(x>18.5 and x<=24.9):
-        print("Normal Weight")
-    elif(x>=25 and x<29.9):
-        print("Over-Weight")
-    elif(x>=29):
-        print("Obese")
+    #print("\n")
+    #print(name,"your body situation is")
+    if(bmi<=18.5):
+        status="Under Weight"
+    elif(bmi>18.5 and bmi<=24.9):
+        status="Normal Weight"
+    elif(bmi>=25 and bmi<29.9):
+        status="Over Weight"
+    elif(bmi>=29):
+        status="Obese"
     #--------------------------
     #to get solution
-    print("\n")
-    print(name,"Your Goal Should Be")
-    if (x <= 18.5):
-        print("High need to increase calories")
-    elif (x > 18.5 and x <= 24.9):
-        print("Mainatain you calories")
-    elif (x >= 25 and x < 29.9):
-        print("Decrease your calories")
-    elif (x >= 29):
-        print("High need to be calorie deficiet")
-
-
+    #print("\n")
+    #print(name,"Your Goal Should Be")
+    if (bmi <= 18.5):
+        solution="High need to increase calories"
+    elif (bmi > 18.5 and bmi <= 24.9):
+        solution="Mainatain you calories"
+    elif (bmi >= 25 and bmi < 29.9):
+        solution="Decrease your calories"
+    elif (bmi >= 29):
+        solution="High need to be calorie deficiet"
+        
+    return bmi,status,solution
+'''
 print("Enter Name")
 name=input()
 print("Enter Age")
@@ -41,4 +53,4 @@ weight=float(input())
 print("Enter Gender")
 gender=input()
 bmi(name,weight,height)
-
+'''
